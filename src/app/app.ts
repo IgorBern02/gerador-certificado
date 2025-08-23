05fe13d312e6a4894d2dba2af254c6824f9929af
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-23 20:07:01 -0300

    feat: adicionado componente certificado conclusão

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,23 @@ import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { BaseUi } from './_components/base-ui/base-ui';
+import { Certificados } from './pages/certificados/certificados';
+import { CertificadoFrom } from './pages/certificado-from/certificado-from';
+import { Certificado } from './pages/certificado/certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [
+    Navbar,
+    PrimaryButton,
+    SecondaryButton,
+    ItemCertificado,
+    BaseUi,
+    Certificados,
+    CertificadoFrom,
+    Certificado,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
