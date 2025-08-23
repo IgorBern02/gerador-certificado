b3bf9e52763a311f1cda86bfb9d89d8282b567fd
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-23 16:51:24 -0300

    feat: adicionado componentes de navbar, primary-button, secondary-button e item-certificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Navbar } from './_components/navbar/navbar';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('gerador-certificado');
